27f2cc8c9da324b89e0733f50d0fc739c54839a1
xaca <xacarana@gmail.com>
2023-07-07 17:56:27 -0500

    Se empieza cambio a firestore para almacenar los datos

--- src/js/modulos/crud/read_firestore.js
+++ src/js/modulos/crud/read_firestore.js
@@ -18,8 +18,29 @@ window.onload = async ()=>{
         pintarMenu();
 
         const querySnapshot = await getDocs(collection(db, "productos"));
+        
         querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
+            //`${doc.id} => ${doc.data()}`
+            pintarPagina(doc.data());
+            //let response = doc.data();
+            //console.log(`${response.nombre} ${response.precio}`);
         });
     }
+}
+
+function pintarPagina(datos){
+    let html;
+    let no_image = "https://icons.iconarchive.com/icons/fatcow/farm-fresh/32/no-image-icon.png";
+    
+    html = `<div class="producto">
+        <div>
+            <h1>${datos.nombre}</h1>
+            <p>${datos.descripcion?datos.descripcion:""}</p>
+        </div>
+        <div>
+            <img src="${datos.url?datos.url:no_image}" />
+        </div>
+    </div>`;
+    
+    galeria.innerHTML = html;
 }
